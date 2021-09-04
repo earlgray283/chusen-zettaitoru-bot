e5b9d59c0b59812ab504f6503770d5ddc7ccacf6
earlgray <earlgray283@gmail.com>
2021-09-04 10:48:10 +0900

    chore: heroku 関連のファイルを削除

--- dotenv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-filename=".env"               # .env の path
-appname="frozen-sierra-65437" # appname
-
-while read line; do
-    echo "line: $line"
-    heroku config:add $line --app $appname
-done < $filename
